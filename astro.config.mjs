a5bd2e5600bf64b5db227ad53ac2904fcf078041
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-09-25 00:33:30 +0200

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
-import { defineConfig } from 'astro/config';
-
-import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: netlify()
-});
+export default defineConfig({});
